211ae99a04d8e07829daaf6a3d73913d350cb83e
duanshilong <shilong.duan@silot.ai>
2020-07-06 11:07:13 +0800

    rename file name

--- src/store/actionCreator.ts
+++ src/store/actionCreator.ts
@@ -1,13 +1,13 @@
 import {TEST_DEMO} from "./actionsTypes"
 import {test} from "../http/api"
 
-export const testRedux = (value) => ({
+export const testRedux = (value: any) => ({
     type: TEST_DEMO,
     value
 })
 
 export const testThunk =() => {
-    return (dispath) => {
+    return (dispath:any) => {
         test({pageNo:1 ,rowNo: 6}).then(({data}) => {
             console.log(data);
             const action = testRedux(data)
